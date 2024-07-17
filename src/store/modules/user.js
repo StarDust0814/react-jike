62e82ea026eeeb0eabe1cc3ef12a62fcd8ac691e
stardust <876700669@qq.com>
2024-07-17 20:54:21 +0800

    add:封装token操作函数

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,17 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { request } from '@/utils';
+import { setToken as _setToken, getToken } from '@/utils';
 
 const userStore = createSlice({
   name: 'user',
   initialState: {
     // 要先校验localstorage中有没有
-    token: localStorage.getItem('token_key') || '',
+    token: getToken() || '',
   },
   reducers: {
     setToken(state, action) {
       state.token = action.payload;
       // localStorage也要保存一份，因为redux是基于浏览器内存的，刷新后token会丢失
-      localStorage.setItem('token_key', action.payload);
+      _setToken(action.payload);
     },
   },
 });
---
+++ src/utils/token.js
@@ -0,0 +1,16 @@
+// 操作localstorage中的token,增删获取
+
+const TOKENKEY = 'token_key';
+function setToken(token) {
+  localStorage.setItem(TOKENKEY, token);
+}
+
+function getToken() {
+  return localStorage.getItem(TOKENKEY);
+}
+
+function removeToken() {
+  localStorage.removeItem(TOKENKEY);
+}
+
+export { setToken, getToken, removeToken };
