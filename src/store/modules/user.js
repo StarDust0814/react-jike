454c97886c30154d3431f64a058c42afc4589037
stardust <876700669@qq.com>
2024-07-17 20:48:04 +0800

    add:token持久化

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -4,11 +4,14 @@ import { request } from '@/utils';
 const userStore = createSlice({
   name: 'user',
   initialState: {
-    token: '',
+    // 要先校验localstorage中有没有
+    token: localStorage.getItem('token_key') || '',
   },
   reducers: {
     setToken(state, action) {
       state.token = action.payload;
+      // localStorage也要保存一份，因为redux是基于浏览器内存的，刷新后token会丢失
+      localStorage.setItem('token_key', action.payload);
     },
   },
 });
