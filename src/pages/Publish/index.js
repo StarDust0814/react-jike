50d65f4cd785e12bdba187d2cec794278f6dea7d
stardust <876700669@qq.com>
2024-07-19 16:39:48 +0800

    add:添加文章发布表单提交

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -15,7 +15,7 @@ import './index.scss';
 import { useEffect, useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-import { getChannelAPI } from '@/apis/article';
+import { createArticleAPI, getChannelAPI } from '@/apis/article';
 
 const { Option } = Select;
 
@@ -27,9 +27,26 @@ const Publish = () => {
       const res = await getChannelAPI();
       setChannelList(res.data.channels);
     };
-
+    // 利用useEffect触发异步获取后端接口
     getChannelList();
   }, []);
+
+  // 提交表单
+  const onFinish = (formValue) => {
+    console.log(formValue);
+    // 根据接口文档格式处理收集到的表单数据
+    const { title, content, channel_id } = formValue;
+    const reqData = {
+      title,
+      content,
+      cover: {
+        type: 0,
+        images: [],
+      },
+      channel_id,
+    };
+    createArticleAPI(reqData);
+  };
 
   return (
     <div className="publish">
@@ -47,6 +64,7 @@ const Publish = () => {
           labelCol={{ span: 4 }}
           wrapperCol={{ span: 16 }}
           initialValues={{ type: 1 }}
+          onFinish={onFinish}
         >
           <Form.Item
             label="标题"
